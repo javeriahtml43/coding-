f860764c4062e30b278d936fc1a787519ea77293
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-15 10:59:54 +0500

    cpp

---
+++ cpp/09.cpp
@@ -0,0 +1,28 @@
+/*
+
+	*******
+	 *****
+	  ***
+	   *
+
+       */
+
+       
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=4;i>=1;i--)
+     {   
+    for(int j=1;j<=4-i;j++)
+       {
+        cout<<" ";
+          }
+          for(int m=1;m<=2*i-1;m++)
+         { 
+             cout<<"*";
+             }
+        cout<<endl;
+   }
+     return 0; 
+}
