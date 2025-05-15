1886f711add07192f1bdb0f393bf6659e806d535
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-15 23:06:54 +0500

    cpp

---
+++ cpp/22.cpp
@@ -0,0 +1,47 @@
+
+
+/*
+
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+    
+ */
+   
+    #include<iostream>
+using namespace std;
+
+int main() 
+{
+    //top
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int s = 4; s >= i; s--)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //bottom
+    for (int i = 3; i >= 1; i--)
+    {
+        for (int s = 4; s >= i; s--)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
