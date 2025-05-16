303c49118f5d5361ef61bac7e3df54ee45b00ce6
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-16 15:08:39 +0500

    cpp

---
+++ cpp/32.cpp
@@ -0,0 +1,36 @@
+	
+	/*
+
+    Repeating character pattern
+	
+
+	A A A A
+	B B B B
+	C C C C
+	D D D D
+
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+     for(char j='A';j<='D';j++)
+
+	 {
+
+		for(char m='A';m<='D';m++)
+		
+		{
+           cout<<j<<" ";
+		}
+
+		cout<<endl;
+
+	 }
+
+	 return 0;
+
+}
