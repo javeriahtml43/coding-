a5e58fc210723b965e976555f164c847ca81a4cb
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-16 21:01:06 +0500

    cpp

---
+++ cpp/40.cpp
@@ -0,0 +1,33 @@
+/*
+
+	 ASCII character pattern
+	
+	
+	65
+	65 66
+	65 66 67
+	65 66 67 68
+	
+    */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 0; i < 4; i++)
+     
+    {
+        for (char ch = 65; ch <= 65 + i; ch++)
+        
+        {
+            cout << (int)ch << " ";
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+
+}
