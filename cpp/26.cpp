9e6a81c4687870cbd2714079d58a045e918c1034
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-16 11:54:19 +0500

    cpp

---
+++ cpp/26.cpp
@@ -0,0 +1,56 @@
+/*
+ 
+  Number diamond
+
+
+1
+121
+12321
+1234321
+12321
+121
+1
+ 
+ */
+
+ #include<iostream>
+using namespace std;
+int main()
+{
+
+     int c=1;
+
+     // upper 
+
+
+    for (int m = 1; m <= 4; m++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout<<j;
+        }
+        for (int j = m-1; j >= 1; j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+     }
+
+     // bottom 
+
+
+     
+    for (int m = 3; m >= 1; m--)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout<<j;
+        }
+        for (int j = m-1; j >= 1; j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+     }
+
+}
