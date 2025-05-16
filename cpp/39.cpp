9b0e3ffd3d3788f61d1b90637850a08983a63f66
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-16 21:33:11 +0500

    cpp

---
+++ cpp/39.cpp
@@ -0,0 +1,46 @@
+
+/*
+
+	9. Uppercase & lowercase mix
+	 
+	A
+	b c
+	D E F
+	g h i j
+
+*/
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    char ch = 'A';
+    
+
+    for (int m=1;m<=4;m++) 
+     {
+
+        for (int j=1;j<=m;j++)
+        
+        {
+            if (m % 2 == 1)
+
+            {
+                cout <<(char)toupper(ch) << " ";
+            }
+
+            else
+  
+            {
+                    cout<<(char)tolower(ch) << " ";
+            }
+
+            ch++;
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
