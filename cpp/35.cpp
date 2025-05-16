af90731c8874c15032434dcc26e42997f19f7266
Javeria Mohsin <javeriaweb43@gmail.com>
2025-05-16 16:08:40 +0500

    cpp

---
+++ cpp/35.cpp
@@ -0,0 +1,38 @@
+/*
+
+	5. Right-aligned alphabet pattern
+
+
+	   A
+	  AB
+	 ABC
+	ABCD
+	
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    for (char j ='A';j<='D';j++)
+   
+    {
+        for (char s ='C';s>=j;s--)
+        
+        {
+            cout<<" ";
+        }
+        
+        for (char m='A';m<=j;m++)
+       
+        {
+           cout<<m;
+        }
+       
+        cout<<endl;
+    
+    }
+    
+    return 0;
+}
